e2a0058491c6392fdb9da161f9fdb7d4c61d55c0
Kevin McWhirter <klmcwhirter@gmail.com>
2018-06-23 14:56:52 -0700

    Initial pieces

---
+++ etc/redis/puzzles2redis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+JQ='[
+    .[] |
+        {
+            name,
+            description,
+            type: (
+                if (.name == "Random" or .name == "Word") then (.name | ascii_downcase)
+                else "static"
+                end
+            ),
+            puzzleWords: (
+                if (.name == "Word") then ["word"]
+                else [.puzzleWords[].word]
+                end
+            )
+        }
+] | sort_by(.name) |
+    map( { (.name|tostring): . } ) | add |
+    keys[] as $k | "\($k) \(.[$k])"
+'
+jq -M -r "${JQ}" puzzles-all.json |
+sed 's/"/\\"/g' |
+awk '
+{
+    key=$1;
+    keys[key]=key;
+    $1="";
+    printf("SET \"urn:puzzle:%s\" \"%s\"\r\n", key, $0);
+}
+END {
+    printf("DEL \"ids:Puzzle\"\r\n");
+    for(k in keys) {
+        printf("SADD \"ids:Puzzle\" \"%s\"\r\n", k);
+    }
+}
+'
